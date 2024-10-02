831b324d70d1af75affd305f0a40ed178f5b907b
Gaurav Pardeshi <120565930+Gaurav051100@users.noreply.github.com>
2024-10-02 19:14:35 +0000

    basics

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountID= 456
+let accountEmail= "gaurav@123"
+var accountPass="gaurav"
+accountCity="SNK"
+console.table([accountID, accountEmail, accountPass,accountCity])
+
+// accountID=753 - Cannot change the values of const 
+
+accountEmail="pardeshi0511"
+accountPass="naina"
+accountCity="Pune"
+
+
+console.table([accountID, accountEmail, accountPass,accountCity])
